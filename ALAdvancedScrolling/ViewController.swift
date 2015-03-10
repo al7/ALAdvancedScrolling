2dc0c74fb9d396176da174c4de66ea09fec5f143
Alex Leite <admin@al7dev.com>
2015-03-09 19:08:03 -0700

    Initial Commit

---
+++ ALAdvancedScrolling/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  ALAdvancedScrolling
+//
+//  Created by Alexandre Leite on 3/9/15.
+//  Copyright (c) 2015 Alexandre Leite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        self.view = UIView()
+        self.view.backgroundColor = UIColor.yellowColor()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
